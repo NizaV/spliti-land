cf850644e8e2d875399fa5dcc77d6cb15db7b6fa
NizaV <60785417+NizaV@users.noreply.github.com>
2020-10-30 17:24:35 -0400

    adding main elements of home

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,22 +7,24 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 export default function Home() {
   return <div className="main">
     <div className="container">
-      <h1 className="text-primary">Hello</h1>
-<div className="apple">
-</div>
-<div className="icon">
+        <div className="apple">
+        </div>
+        <div className="icon">
 
-</div>
-<div className="slogan">
-  {"AN EASIER WAY TO SPLIT"}
-</div>
-<p className="slogan-child">{"Purchases appear directly in the app. Tag in your friends, let"}<span style={{fontWeight: '700'}}>{"Spliti"}</span>{"do the math or let your friends pay their own amounts. Try out"}<span style={{fontWeight: '700'}}>{"Autopilot"}</span>{", a hands free splitting experience for recurring splitting!"}</p>
-<div className="samsung">
+        </div>
+        <div className="slogan">
+          {"AN EASIER WAY TO SPLIT"}
+        </div>
+        <p className="slogan-child">{"Purchases appear directly in the app. Tag in your friends, let"}<span style={{fontWeight: '700'}}>{" Spliti "}</span>{"do the math or let your friends pay their own amounts. Try out"}<span style={{fontWeight: '700'}}>{" Autopilot"}</span>{", a hands free splitting experience for recurring splitting!"}</p>
+        <input className="email"></input>
+        <Link to="/confirm">
+          <button className="access">
+             <p className="access-text"> {"Get Early Access"} </p>
+            </button>
+        </Link>
+        <div className="samsung">
 
-</div>
-      <Link to="/confirm">
-        <button> Confirm</button>
-      </Link>
+        </div>
     </div>
   </div>
 }
