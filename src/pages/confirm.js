67f77492473f5ee769f34c70f4828dbdacf4d313
NizaV <60785417+NizaV@users.noreply.github.com>
2020-10-30 01:29:12 -0400

    intial commit

---
+++ src/pages/confirm.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const ConfirmPage = () => (
+  <main>
+    <h1>Confirmation Page</h1>
+    <p>Thanks</p>
+  </main>
+)
+export default ConfirmPage;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,15 @@
-import React from "react"
+import React from "react";
+import { Link } from "gatsby"
+// import '../styles/index.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <div>
+    <div className="container">
+      <h1 className="text-primary">Hello</h1>
+      <Link to="/confirm">
+        <button> Confirm</button>
+      </Link>
+    </div>
+  </div>
 }
