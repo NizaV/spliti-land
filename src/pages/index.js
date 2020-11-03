70cc39a7330033684c83a4d24d14ec9f47a555f3
NizaV <60785417+NizaV@users.noreply.github.com>
2020-11-03 10:18:30 -0500

    attempting new text rotator plugin

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,11 @@ import { Link } from "gatsby"
 import '../styles/index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { TextRotator } from './textRotator'
+import { Word } from './word'
 
 
-export default function Home() {
-  return <div className="main">
+export default function Home() { 
+  return  <div className="main">
     <div className="container">
         <div className="apple">
         </div>
@@ -19,9 +20,10 @@ export default function Home() {
         <p className="slogan-child">{"Purchases appear directly in the app. Tag in your friends, let"}<span style={{fontWeight: '700'}}>{" Spliti "}</span>{"do the math or let your friends pay their own amounts. Try out"}<span style={{fontWeight: '700'}}>{" Autopilot"}</span>{", a hands free splitting experience for recurring splitting!"}</p>
         <div className="container">
         <TextRotator/>
+        <Word/>
         </div>
         <input className="email"></input>
-        <Link to="/confirm">
+        <Link to="/word/">
           <button className="access">
              <p className="access-text"> {"Get Early Access"} </p>
             </button>
@@ -31,4 +33,5 @@ export default function Home() {
         </div>
     </div>
   </div>
+  
 }
