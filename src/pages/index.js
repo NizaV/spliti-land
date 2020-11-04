33802208645d0869ab6b7afb4e41cb141745bb8a
NizaV <60785417+NizaV@users.noreply.github.com>
2020-11-03 23:47:07 -0500

    testing out options for typewriter

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import { Link } from "gatsby"
 import '../styles/index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { TextRotator } from './textRotator'
-
 import Typewriter from 'typewriter-effect';
 
 
@@ -21,16 +20,37 @@ export default function Home() {
         <p className="slogan-child">{"Purchases appear directly in the app. Tag in your friends, let"}<span style={{fontWeight: '700'}}>{" Spliti "}</span>{"do the math or let your friends pay their own amounts. Try out"}<span style={{fontWeight: '700'}}>{" Autopilot"}</span>{", a hands free splitting experience for recurring splitting!"}</p>
         <div className="container">
         {/* <TextRotator/> */}
-        <div className="text-rotator">
-        <Typewriter
+        
+        {/* <Typewriter
           options={{
             strings: ['UTILITIES', 'PHONE', 'NETFLIX', 'HULU', 'RENT', 'FOOD', 'DRINKS', 'TRIPS', 'FLIGHTS', 'MOVIES'],
             autoStart: true,
             loop: true,
-            changeDeleteSpeed: 2,
+            deleteSpeed: 1,
+            cursor: '$',
+            wrapperClassName: "text-rotator",
           }}
-        />
+        /> */}
+        <div className="text-rotator">
+        <Typewriter
+            onInit={(typewriter) => {
+              typewriter.changeCursor('$')
+              .typeString('UTILITIES')
+              .pauseFor(500)
+              .deleteAll()
+              .start();
+            }}
+            // options={{
+            //   strings: ['UTILITIES', 'PHONE', 'NETFLIX', 'HULU', 'RENT', 'FOOD', 'DRINKS', 'TRIPS', 'FLIGHTS', 'MOVIES'],
+            //   autoStart: true,
+            //   loop: true,
+            //   deleteSpeed: 1,
+            //   cursor: null,
+            //   wrapperClassName: "text-rotator",
+            // }}
+          />
         </div>
+      
         </div>
         <input className="email"></input>
         <Link to="/word/">
