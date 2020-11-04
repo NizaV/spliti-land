e17cd2a625decca96ab44858c705b6cb6248dbe0
NizaV <60785417+NizaV@users.noreply.github.com>
2020-11-03 23:04:06 -0500

    integrated typewriter

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import { Link } from "gatsby"
 import '../styles/index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { TextRotator } from './textRotator'
-import { Word } from './word'
+
 import Typewriter from 'typewriter-effect';
 
 
@@ -20,21 +20,17 @@ export default function Home() {
         </div>
         <p className="slogan-child">{"Purchases appear directly in the app. Tag in your friends, let"}<span style={{fontWeight: '700'}}>{" Spliti "}</span>{"do the math or let your friends pay their own amounts. Try out"}<span style={{fontWeight: '700'}}>{" Autopilot"}</span>{", a hands free splitting experience for recurring splitting!"}</p>
         <div className="container">
-        <TextRotator/>
+        {/* <TextRotator/> */}
+        <div className="text-rotator">
         <Typewriter
-          onInit={(typewriter) => {
-            typewriter.typeString('Hello World!')
-              .callFunction(() => {
-                console.log('String typed out!');
-              })
-              .pauseFor(2500)
-              .deleteAll()
-              .callFunction(() => {
-                console.log('All strings were deleted');
-              })
-              .start();
+          options={{
+            strings: ['UTILITIES', 'PHONE', 'NETFLIX', 'HULU', 'RENT', 'FOOD', 'DRINKS', 'TRIPS', 'FLIGHTS', 'MOVIES'],
+            autoStart: true,
+            loop: true,
+            changeDeleteSpeed: 2,
           }}
         />
+        </div>
         </div>
         <input className="email"></input>
         <Link to="/word/">
