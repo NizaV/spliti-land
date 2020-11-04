c46a85a8a99b273abb6546d8ef30228ffe2ddcc6
NizaV <60785417+NizaV@users.noreply.github.com>
2020-11-03 22:50:09 -0500

    added new typewriter effect

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import '../styles/index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { TextRotator } from './textRotator'
 import { Word } from './word'
+import Typewriter from 'typewriter-effect';
 
 
 export default function Home() { 
@@ -20,7 +21,20 @@ export default function Home() {
         <p className="slogan-child">{"Purchases appear directly in the app. Tag in your friends, let"}<span style={{fontWeight: '700'}}>{" Spliti "}</span>{"do the math or let your friends pay their own amounts. Try out"}<span style={{fontWeight: '700'}}>{" Autopilot"}</span>{", a hands free splitting experience for recurring splitting!"}</p>
         <div className="container">
         <TextRotator/>
-        <Word/>
+        <Typewriter
+          onInit={(typewriter) => {
+            typewriter.typeString('Hello World!')
+              .callFunction(() => {
+                console.log('String typed out!');
+              })
+              .pauseFor(2500)
+              .deleteAll()
+              .callFunction(() => {
+                console.log('All strings were deleted');
+              })
+              .start();
+          }}
+        />
         </div>
         <input className="email"></input>
         <Link to="/word/">
