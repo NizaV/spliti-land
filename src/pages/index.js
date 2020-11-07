1cb2bc99d91fdb12fc6a035251aba05dcbe1c474
NizaV <60785417+NizaV@users.noreply.github.com>
2020-11-06 22:40:35 -0500

    adding onclick function for button change

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {useState} from "react"
 import { Link } from "gatsby"
 import '../styles/index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -6,6 +6,18 @@ import Typewriter from 'typewriter-effect';
 
 
 export default function Home() { 
+  const [color, setColor] = useState("#FF6E34");
+  const [textColor, setTextColor] = useState("white");
+  const [buttonText, setButtonText] = useState("Get Early Access")
+  const handleClick = () => {
+    setColor('white');
+    setTextColor('#FF6E34');
+    setButtonText('Sent');
+  }
+  const buttonStyle = {
+    backgroundColor: color,
+    color: textColor
+  }
   return  <div className="main">
     <div className="container">
         <div className="apple">
@@ -18,48 +30,24 @@ export default function Home() {
         </div>
         <p className="slogan-child">{"Purchases appear directly in the app. Tag in your friends, let"}<span style={{fontWeight: '700'}}>{" Spliti "}</span>{"do the math or let your friends pay their own amounts. Try out"}<span style={{fontWeight: '700'}}>{" Autopilot"}</span>{", a hands free splitting experience for recurring splitting!"}</p>
         <div className="container">
-        {/* <TextRotator/> */}
         
-        {/* <Typewriter
+        <Typewriter
           options={{
             strings: ['UTILITIES', 'PHONE', 'NETFLIX', 'HULU', 'RENT', 'FOOD', 'DRINKS', 'TRIPS', 'FLIGHTS', 'MOVIES'],
             autoStart: true,
             loop: true,
             deleteSpeed: 1,
-            cursor: '$',
+            delay: 50,
             wrapperClassName: "text-rotator",
           }}
-        /> */}
-        <div className="text-rotator">
-        <Typewriter
-            // onInit={(typewriter) => {
-            //   typewriter.changeCursor('$')
-            //   .typeString('UTILITIES')
-            //   .pauseFor(300)
-            //   .deleteAll()
-            //   .start()
-            //   .typeString('PHONE')
-            //   .pauseFor(300)
-            //   .deleteAll()
-            //   .typeString('NETFLIX')
-            //   .pauseFor(300)
-            //   .deleteAll();
-            // }}
-            options={{
-              strings: ['UTILITIES', 'PHONE', 'NETFLIX', 'HULU', 'RENT', 'FOOD', 'DRINKS', 'TRIPS', 'FLIGHTS', 'MOVIES'],
-              autoStart: true,
-              loop: true,
-              deleteSpeed: 1,
-              cursor: null,
-              wrapperClassName: "text-rotator",
-            }}
-          />
-        </div>
+        />
+       
       
         </div>
+        
         <input className="email"></input>
-          <button className="access">
-             <p className="access-text"> {"Get Early Access"} </p>
+          <button className="access" id="myButton1" style={buttonStyle} onClick={handleClick}>
+             <p className="access-text"> {buttonText} </p>
             </button>
         <div className="samsung">
 
